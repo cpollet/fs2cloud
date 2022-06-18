a08eefe41669611eac5dd0802a31047b628ead7c
Christophe Pollet <cpollet@users.noreply.github.com>
2022-06-18 10:43:31 +0200

    Sort inodes by id

--- src/sql/inode_find_by_parent_id_and_name.sql
+++ src/sql/inode_find_by_parent_id_and_name.sql
@@ -2,3 +2,4 @@ select id, parent_id, file_uuid, name
 from inodes
 where parent_id = :parent_id
   and name = :name
+order by id
--- src/sql/inode_list_by_parent_id.sql
+++ src/sql/inode_list_by_parent_id.sql
@@ -1,3 +1,4 @@
 select id, parent_id, file_uuid, name
 from inodes
 where parent_id = :parent_id
+order by id
