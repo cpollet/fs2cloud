06558d16020181db9339f98174daa61c24303b8e
Christophe Pollet <cpollet@users.noreply.github.com>
2022-11-08 20:24:57 +0100

    Fix test shell scripts

--- fs.sh
+++ fs.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+release=1
+
 export RUST_LOG="warn,fs2cloud::controller::mount=trace"
 mkdir -p /tmp/fuse
 sudo umount /tmp/fuse
-cargo run -- -c test/config.yml mount -m /tmp/fuse
+
+if [ $release -eq 0 ]; then
+  echo "debug mode"
+  cargo run -- -c test/config.yml mount -m /tmp/fuse
+else
+  echo "release mode"
+  cargo run --release -- -c test/config.yml mount -m /tmp/fuse
+fi
--- push.sh
+++ push.sh
@@ -5,9 +5,11 @@ export RUST_LOG="warn,fs2cloud=info,fs2cloud::store::s3_official=trace"
 rm test/database.db3
 
 if [ $release -eq 0 ]; then
+  echo "debug mode"
   cargo build
   time target/debug/fs2cloud -c test/config.yml push
 else
+  echo "release mode"
   cargo build --release
-  time target/debug/fs2cloud -c test/config.yml push
+  time target/release/fs2cloud -c test/config.yml push
 fi
